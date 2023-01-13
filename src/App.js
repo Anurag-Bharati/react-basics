7f3be71a778cafd55a60ad9a35d8cfef516e046e
Anurag-Bharati <anuragbharati26@gmail.com>
2023-01-13 09:32:00 +0545

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,60 @@ import "./App.css";
 function App() {
     const [newNote, setNewNote] = useState();
     const [notes, setNotes] = useState([]);
+    const [showAll, setShowAll] = useState(true);
+    const notesToShow = showAll
+        ? notes
+        : notes.filter((n) => n.important == true);
+
     useEffect(() => {
         axios
             .get("http://localhost:3001/notes")
             .then((res) => setNotes(res.data));
     }, []);
+    const handleDelete = (id) => {
+        if (window.confirm("do you relly want to delete this note?")) {
+        }
+    };
     const handleInputChange = (event) => {
         console.log(event.target.value);
+        setNewNote(event.target.value);
     };
-    const handleAdd = (event) => event.preventDefault();
+    const handleAdd = (event) => {
+        event.preventDefault();
+        const note = {
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toString(),
+            important: Math.random() < 0.5,
+        };
+        if (newNote !== "") setNotes(notes.concat(note));
+    };
+    const handleImpAdd = (event) => {
+        event.preventDefault();
+        const note = {
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toString(),
+            important: true,
+        };
+        if (newNote !== "") setNotes(notes.concat(note));
+    };
 
-    // return <>{Feedback()}</>;
     return (
         <>
             <h2>Notes</h2>
+            <button onClick={() => setShowAll(!showAll)}>
+                {showAll ? "Show Important" : "Show All"}
+            </button>
             <ul>
-                {notes.map((note) => (
+                {notesToShow.map((note) => (
                     <li key={note.id}>{note.content}</li>
                 ))}
             </ul>
             <form>
                 <input value={newNote} onChange={handleInputChange} />
                 <button onClick={handleAdd}>add</button>
+                <button onClick={handleImpAdd}>add important</button>
             </form>
         </>
     );
